4b33fee44c5deebf862024fadbd6b1d931cf554b
Stefano Baruzzo <stefanone_91@hotmail.it>
2022-03-30 10:28:04 +0200

    updated to .net 6

--- HtmlPdfApi/Program.cs
+++ HtmlPdfApi/Program.cs
@@ -1,26 +1,126 @@
-using System.Diagnostics.CodeAnalysis;
+using System;
+using HtmlPdfApi.Helpers.Exceptions;
+using HtmlPdfApi.Helpers.HealthChecks;
+using HtmlPdfApi.Helpers.Http;
+using HtmlPdfApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace HtmlPdfApi
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure app configuration
+builder.Host.ConfigureAppConfiguration(configurationBuilder =>
 {
-    [ExcludeFromCodeCoverage]
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    DotNetEnv.Env.Load();
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(configurationBuilder =>
+// Configure services
+builder.Host.ConfigureServices(services =>
+{
+    var Configuration = builder.Configuration;
+
+    // Singleton, transient, scoped
+    services.TryAddSingleton<ResourceService>();
+    services.TryAddSingleton<HandlebarsService>();
+    services.TryAddSingleton<HtmlPdfService>();
+
+    // Http base client
+    services.AddHttpClient("default")
+        .ConfigurePrimaryHttpMessageHandler(() => new HttpClientCertificateHandler(Configuration.GetValue<bool>("CERTIFICATE_VALIDATION_ENABLED")));
+
+    // Auth enabled
+    if (Configuration.GetValue<bool>("OIDC_AUTH_ENABLED") == true)
+    {
+        IdentityModelEventSource.ShowPII = true; //Add this line
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
                 {
-                    DotNetEnv.Env.Load();
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
+                    options.Authority = Configuration.GetValue<string>("OIDC_ISSUER");
+                    options.RequireHttpsMetadata = Configuration.GetValue<bool>("CERTIFICATE_VALIDATION_ENABLED");
+                    options.BackchannelHttpHandler = new HttpClientCertificateHandler(Configuration.GetValue<bool>("CERTIFICATE_VALIDATION_ENABLED"));
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false,
+                        ValidateIssuer = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration.GetValue<string>("OIDC_ISSUER")
+                    };
                 });
     }
+    // If auth disabled returns true to avery [Authorize] request
+    else
+    {
+        services.AddAuthorization(x =>
+            {
+                x.DefaultPolicy = new AuthorizationPolicyBuilder()
+                    .RequireAssertion(_ => true)
+                    .Build();
+            });
+    }
+
+    // Health checks
+    services.AddHealthChecks()
+        .AddCheck<KeycloakHealthCheck>("keycloak_health_check");
+
+    // Controllers
+    services.AddControllers().AddNewtonsoftJson();
+
+    // Swagger
+    services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Investigations API",
+                Description = "Investigations API description",
+                Contact = new OpenApiContact
+                {
+                    Name = "Faberbee S.r.l.",
+                    Email = "support@faberbee.com",
+                    Url = new Uri("https://www.faberbee.com"),
+                }
+            });
+        });
+
+});
+
+// Configure app
+var app = builder.Build();
+
+#region CONFIGURE APP
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Investigations API V1");
+});
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHealthChecks("/health");
+    endpoints.MapControllers();
+});
+#endregion
+
+app.Run();
